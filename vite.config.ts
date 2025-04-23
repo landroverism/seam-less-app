80e51aad233a60223ee80ed5a80bc83067e3f3f2
volkswagen <landroverism@kali.hamkemboi>
2025-04-24 01:01:48 +0300

    done with deploy

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,30 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import react from '@vitejs/plugin-react'
 import path from 'path'
 
-export default defineConfig({
-  plugins: [vue()],
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, './src'),
+export default defineConfig(({ mode }) => {
+  return {
+    plugins: [
+      mode === 'react' ? react() : vue()
+    ],
+    resolve: {
+      alias: {
+        '@': path.resolve(__dirname, './src')
+      },
+      extensions: ['.mjs', '.js', '.ts', '.jsx', '.tsx', '.json', '.vue']
     },
-  },
+    build: {
+      outDir: mode === 'react' ? 'dist-react' : 'dist',
+      emptyOutDir: true,
+      rollupOptions: {
+        input: {
+          main: path.resolve(__dirname, 'index.html')
+        }
+      }
+    },
+    optimizeDeps: {
+      include: ['vue', 'vue-router', 'pinia', 'element-plus', 'react', 'react-dom']
+    }
+  }
 })
